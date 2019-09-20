eab7e59e8016fd0b07c2febac74d1bc80f09fa15
NEC007 <38103935+NEC007@users.noreply.github.com>
2019-09-20 14:03:02 +0200

    move

--- script/control.js
+++ script/control.js
@@ -1,58 +1,41 @@
 document.addEventListener("keydown", move);
 
-let topValue = 40;
-let downValue = 40;
-let leftValue = 40;
-let rightValue = 40;
+let horizental = 40;
+let vertical = 40;
 
 function move(e) {
-    console.log(event.code);
+    console.log(event.keyCode);
     switch(e.keyCode){
         case 37:{
-            topValue +=10;
+            horizental -=10;
             break;
         }
         case 38:{
-            downValue +=10;
+            vertical -=10;
             break;
         }
         case 39:{
-            leftValue +=10;
+            horizental +=10;
             break;
         }
         case 40:{
-            rightValue +=10;
+            vertical +=10;
             break;
         }
+        default: 
+            break;
     }
     
     
-    /*
-    if (event.keyCode == "38") {
-        step += 10;
-    }else if (event.keyCode == "37"){
-        step +=10;
+
+    if (horizental > 90) {
+        horizental = 0;
+    }else if (vertical >90){
+        vertical =0;
     }
-    else if (event.keyCode == "40"){
-        step +=10;
-    }else if (event.keyCode == "39"){
-        step += 10;
-    }
-    */
-    /*
-    if (topValue > 90) {
-        topValue = 0;
-    }else if (downValue >90){
-        downValue =0;
-    }else if(leftValue > 90){
-        leftValue = 0;
-    }else if(rightValue > 90){
-        rightValue = 0;
-    }
-    */
     
-    document.querySelector("div").style.top = topValue + '%';
-    document.querySelector("div").style.bottom = downValue + '%';
-    document.querySelector("div").style.left = leftValue + '%';
-    document.querySelector("div").style.right = rightValue + '%';
+    
+    document.querySelector("div").style.top = vertical + '%';
+    document.querySelector("div").style.left = horizental + '%';
+   
 }
